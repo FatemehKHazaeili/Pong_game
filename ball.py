85cf3233b270f794eba4e23e909bcc8ac1784cb8
Fatemeh Khazaeili <fatemeh.khazaeili.6@gmail.com>
2025-09-04 21:43:34 +0330

    Finished

--- ball.py
+++ ball.py
@@ -1,8 +1,5 @@
 from turtle import Turtle
 
-import paddle
-
-
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -10,19 +7,22 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.speed(1)
+        self.speed_range = 1
         self.setheading(45)
 
     def collision_with_wall(self):
         if self.ycor() > 240 or self.ycor() < -230:
             self.speed(0)
             self.setheading(self.heading() * -1)
-            self.speed(1)
+            self.speed(self.speed_range)
 
     def collision_with_paddle(self , paddle):
-        if self.distance(paddle) < 20:
-            self.speed(0)
-            self.setheading(self.heading() * -1 + 180)
-            self.speed(1)
+        if self.xcor() > 360 or self.xcor() < -370:
+            if self.distance(paddle) < 30:
+                self.speed(0)
+                self.speed_range += 0.2
+                self.setheading(self.heading() * -1 + 180)
+                self.speed(self.speed_range)
 
     def check_collision(self ,right ,left):
         self.collision_with_paddle(right)
@@ -41,5 +41,7 @@ class Ball(Turtle):
     def start_over(self):
         self.hideturtle()
         self.goto(0, 0)
-        self.setheading(45)
+        self.setheading(self.heading() * -1 + 180)
+        self.speed(1)
+        self.speed_range = 1
         self.showturtle()
